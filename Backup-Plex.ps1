19e8d73dc9cc8fd9ab3038df309671981ad06b0e
Simon Tims <si@simontims.com>
2018-12-23 20:38:09 +1300

    Create Backup-Plex.ps1

---
+++ Backup-Plex.ps1
@@ -0,0 +1,37 @@
+# Backup-Plex
+# Simon Tims
+
+# Settings
+$mirror = "C:\path\to\mirror"
+$destination = "\\path\to\destination"
+$tempLocation = "C:\temp"
+
+# Do not change anything below this line
+##################################################
+
+
+
+# Load ZipFile .NET Framework
+Add-Type -assembly "system.io.compression.filesystem"
+
+# Build paths
+$regKey = "HKEY_CURRENT_USER\Software\Plex, Inc.\Plex Media Server"
+$source = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Plex Media Server"
+$cacheFolder = Join-Path -Path $source -ChildPath "cache"
+$regKeyFile = Join-Path -Path $mirror -ChildPath "PlexRegistryKey.reg"
+$zipFile = Join-Path -Path $tempLocation -ChildPath "Plex.zip"
+
+# Mirror Plex data folder
+robocopy $source $mirror /MIR /NFL /NDL /R:0 /W:0 /XD $cacheFolder
+
+# Export Plex Registry key 
+Reg export $regKey $regKeyFile
+
+# Zip file should have been removed already; double check and remove if present
+if (Test-Path $zipFile) {Remove-Item $zipFile}
+
+# Zip folder to zipfile (cannot be the source folder we are zipping)
+[io.compression.zipfile]::CreateFromDirectory($mirror, $zipFile)
+
+# Move zipfile to destination
+Move-Item $zipFile $destination -Force
